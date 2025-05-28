169604342161e229f430b46f77acdb7927092a46
Bence Szabó <bszabo96@gmail.com>
2025-05-28 13:54:20 +0200

    set iterm settings in mac script

--- scripts/mac.sh
+++ scripts/mac.sh
@@ -3,6 +3,10 @@ brew install visual-studio-code
 brew install google-chrome
 brew install vlc
 
+brew install --cask iterm2
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.config/iterm2/custom_settings"
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+
 git config --global user.email "bszabo96@gmail.com"
 git config --global user.name "Bence Szabó"
 
