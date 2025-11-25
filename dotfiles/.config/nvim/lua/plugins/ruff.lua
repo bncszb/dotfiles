b304872a23caed5b483f293f547d2758c7759dc5
Bence Szabó <szabo.bence11@ext.telekom.hu>
2025-11-25 09:57:55 +0100

    fix pyproject path

--- dotfiles/.config/nvim/lua/plugins/ruff.lua
+++ dotfiles/.config/nvim/lua/plugins/ruff.lua
@@ -6,7 +6,7 @@ return {
   vim.lsp.config("ruff", {
     init_options = {
       settings = {
-        configuration = vim.fn.getcwd() .. "pyproject.toml",
+        configuration = vim.fn.getcwd() .. "/pyproject.toml",
       },
     },
     root_dir = vim.fn.getcwd(),
