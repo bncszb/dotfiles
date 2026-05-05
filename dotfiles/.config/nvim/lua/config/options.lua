7b047a5530eb8811fc704c1fcf68c0ef96e89085
Bence Szabó <bszabo96@gmail.com>
2026-05-05 16:42:51 +0200

    remove pyright from nvim

--- dotfiles/.config/nvim/lua/config/options.lua
+++ dotfiles/.config/nvim/lua/config/options.lua
@@ -4,8 +4,6 @@
 
 vim.g.autoformat = true
 -- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = "pyright"
 -- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
 
